7216c10eff2f58fa5ce6f011828422887406ad79
“kabilanvel22” <kabilan220823@gmail.com>
2024-03-02 11:17:21 +0530

    first commit

---
+++ homeGarden/src/main/java/com/HomeGardenApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeGardenApplication.class, args);
+	}
+
+}
